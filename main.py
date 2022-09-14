c981945d1bea9abb4de9b966627588b3314ba513
Yinan Qin <xn--15tt45a@xn--bbr.xn--fiqs8s>
2022-09-14 15:35:58 +0800

    Add GUI Branch.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import core.utils as utils
 import sys
 sys.setrecursionlimit(2147483647)
 
-x = utils.SWJTUCalendar("/Users/ghayinanoghaqino/Downloads/学生个人信息-VATUU为途教学信息服务平台.html")
+x = utils.SWJTUCalendar("/Users/ghayinanoghaqino/Downloads/5.html")
 x.save_calendar()
 
