c3593971c6acbb991343a512bbce5ffbc20f7aac
Yinan Qin <xn--15tt45a@xn--bbr.xn--fiqs8s>
2022-10-16 10:37:28 +0800

    更正地点不同而代码相同、日期相同的课程会识别为一个课程的问题 添加按周次下载功能 将导出文件名称有好化，改为课程名称。

--- utils/saveclasstable.py
+++ utils/saveclasstable.py
@@ -15,3 +15,18 @@ def save_all_table_html():
             f.writelines(tabel)
         print("[ info ] Saved table: ", i)
     return temp_path
+
+
+def save_given_week_table_html(week_num):
+    cookies = webtools.get_cookie()
+    temp_path = None
+
+    tabel = webtools.get_class_table(cookies, week_num)
+    filename = str(week_num) + ".html"
+    temp_path = os.path.join(os.getcwd(), "temp")
+    os.makedirs(temp_path, exist_ok=True)
+    filename = os.path.join(temp_path, filename)
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.writelines(tabel)
+    print("[ info ] Saved table: ", week_num)
+    return temp_path
