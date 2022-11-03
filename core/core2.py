cf7305c915a9b6bd60c4c0d3d60aa86b31d548e2
⁽⁽ଘ以̌南̌ଓ⁾⁾ ⁽⁽ଘ秦̌ଓ⁾⁾ <1522846127@qq.com>
2022-11-03 20:06:06 +0800

    Add some classes.

--- core/core2.py
+++ core/core2.py
@@ -1,6 +1,8 @@
-from core.utils import ClassTableHTML
+from core.utils import ClassTableHTML, ClassTableInfo
 
 
 class SWJTUCalendar:
     def __init__(self, filepath, week):
         self.html = ClassTableHTML(filepath, week)
+        self.classtable = ClassTableInfo(self.html)
+
