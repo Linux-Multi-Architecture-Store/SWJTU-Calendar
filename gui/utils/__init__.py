9182c0f61090cb680f1de258181312169f365f39
⁽⁽ଘ以̌南̌ଓ⁾⁾ ⁽⁽ଘ秦̌ଓ⁾⁾ <1522846127@qq.com>
2022-11-11 16:13:12 +0800

    Roughly finish the app.

--- gui/utils/__init__.py
+++ gui/utils/__init__.py
@@ -3,4 +3,5 @@ from .Menu import add_menu
 from .Windows import about_programme
 from .extras import *
 from .ChoosePath import store_selected_saving_path
-__all__ = ['add_fonts_and_dpi', 'add_menu', 'about_programme']
+from .RunMainProgramme import StartProcess
+__all__ = ['add_fonts_and_dpi', 'add_menu', 'about_programme', 'StartProcess']
