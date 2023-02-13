77950d0b71e03982e274cf0413c33ecd1219a808
Yinan Qin <ghayinanoghaqino@icloud.com>
2023-02-13 22:15:23 +0800

    move file.

--- unical/main/__init__.py
+++ unical/main/__init__.py
@@ -1,8 +1,6 @@
 from . import database
-from .saveclasstable import save_all_table_html, save_given_week_table_html
 from .base import ClassInfo, ClassTime, ClassTableHTML, ClassTableInfo
-from .constants import START_AND_END_TIMES
-from .logger import Logger
+from unical.main.utils.logger import Logger
 __all__ = [
     "save_all_table_html", "save_given_week_table_html",
     "ClassInfo", "ClassTime", "ClassTableHTML", "ClassTableInfo",
